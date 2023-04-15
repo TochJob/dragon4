263f3a579a078fe55500831615172db45a38d8c8
TochJob <tochjob@gmail.com>
2023-04-15 21:48:43 +0300

    add:next section

---
+++ src/views/BonusView.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="">
+    <HeaderSection title="Драгон Мани" desc="Популярнейший игровой портал, позволяющий зарабатывать и выводить реальные деньги."/>
+  </section>
+</template>
+<script>
+import HeaderSection from '@/components/HeaderSection.vue';
+
+export default { components: { HeaderSection } };
+</script>
+<style lang="scss">
+  
+</style>
